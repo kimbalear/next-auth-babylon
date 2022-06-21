eef7049095bebbce64d4216e1a5754d3f3a0e989
Joaquin Urbano Serra <kimbalear@gmail.com>
2022-06-21 17:24:28 +0200

    login mobile

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,13 @@
 import { signIn, useSession, getProviders } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
-
 function LoginPage() {
 
     const { data: session, status } = useSession()
     const router = useRouter()
+
+    let w_image = '100%'
+    let h_image = '100%'
 
     if (status !== 'loading' && status === 'authenticated') {
         router.push('/')
@@ -13,7 +15,11 @@ function LoginPage() {
 
     return (
         <div className='login'>
+            <div className='login_up'>
+                <img src='/login_up.jpg' width={w_image} />
+            </div>
             <div className='form'>
+                <img className='logo' src='/logo.png' width={36} height={36} />
                 <h1>Create an account</h1>
                 <div class="field">
                     <label>Phone Number:</label>
@@ -25,6 +31,10 @@ function LoginPage() {
                     <input type="text" required placeholder='Enter Your Name' />
                 </div>
                 <div class="field">
+                    <label>Sex at birth:</label>
+                    <input type="text" required placeholder='Enter Your sex' />
+                </div>
+                <div class="field">
                     <label>Birth Date:</label>
                     <input type="date" required placeholder='Enter Your birth date' />
                 </div>
@@ -33,6 +43,7 @@ function LoginPage() {
                 </button>
             </div>
         </div>
+
     )
 }
 
